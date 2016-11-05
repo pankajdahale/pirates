228287c7d4c46631ace104fa549be2bb573863d1
Pankaj <Pankaj@DESKTOP-RFG6KN5>
2016-11-05 18:05:48 +0530

    changes made in namespace and model folder

--- Pirates/Pirates.API/Controllers/StateController.cs
+++ Pirates/Pirates.API/Controllers/StateController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Data;
+using Pirates.API.Model;
 
 namespace Pirates.API.Controllers
 {
@@ -28,16 +29,5 @@ namespace Pirates.API.Controllers
             return Ok();
         }
 
-    }
-
-    public class DetailVM
-    {
-        public string Name { get; set; }
-        public string VehicleNumber { get; set; }
-        public string MobileNumber { get; set; }
-        public string EmailAddress { get; set; }
-        public string TicketNumber { get; set; }
-        public string PolicePersonName { get; set; }
-        public int StateId { get; set; }
     }
 }
---
+++ Pirates/Pirates.API/Model/DetailVM.cs
@@ -0,0 +1,15 @@
+﻿namespace Pirates.API.Model
+
+
+{
+    public class DetailVM
+    {
+        public string Name { get; set; }
+        public string VehicleNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string TicketNumber { get; set; }
+        public string PolicePersonName { get; set; }
+        public int StateId { get; set; }
+    }
+}
