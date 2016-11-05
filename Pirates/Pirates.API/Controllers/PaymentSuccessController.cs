08551cc0bd1c14a1acbc3b19e2ea5cb93391783f
Pankaj <Pankaj@DESKTOP-RFG6KN5>
2016-11-05 21:10:01 +0530

    All the API done exept Payment

---
+++ Pirates/Pirates.API/Controllers/PaymentSuccessController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Pirates.API.Model;
+
+namespace Pirates.API.Controllers
+{
+    public class PaymentSuccessController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Ok(new List<PaymentSuccess>()
+            {
+                 new PaymentSuccess { ConfirmtionNumber = "100", PaymentDate = "16/10/2016", AmountPaid = "100" },
+
+
+            });
+
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Detail(PaymentSuccess vm)
+        {
+            return Ok();
+
+        }
+    }
+}
