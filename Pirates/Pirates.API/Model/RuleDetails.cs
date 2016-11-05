dcf885a62a58d668e96fff7f3308de5efd010686
Pankaj <Pankaj@DESKTOP-RFG6KN5>
2016-11-05 19:06:28 +0530

    second api rulelist is done

---
+++ Pirates/Pirates.API/Model/RuleDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pirates.API.Model
+{
+    public class RuleDetails
+    {
+        public int Id  { get; set; }
+        public string ListOfRules { get; set; }
+        public string Pay { get; set; }
+       
+    }
+}
